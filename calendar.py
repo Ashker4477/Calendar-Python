a8b393fd180b71cc347fe6f022ff47aa029c75b1
muhammed Ashker <muhammedashker3@gmail.com>
2020-12-18 08:59:07 +0530

    calendar func to find day is made

---
+++ calendar.py
@@ -0,0 +1,85 @@
+def day():
+    d={1:'tuesday',2:'wednesday',3:'thursday',4:'friday',5:'saturday',6:'sunday',7:'monday'}
+    while True:
+        c=int(input("enter the date"))
+        if c==1:
+            print(d[1])
+        if c==2:
+            print(d[2])
+        if c==3:
+            print(d[3])
+        if c==4:
+            print(d[4])
+        if c==5:
+            print(d[5])
+        if c==6:
+            print(d[6])
+        if c==7:
+            print(d[7])
+        if c==8:
+            print(d[1])
+        if c==9:
+            print(d[2])
+        if c==10:
+            print(d[3])
+        if c==11:
+            print(d[4])
+        if c==12:
+            print(d[5])
+        if c==13:
+            print(d[6])
+        if c==14:
+            print(d[7])
+        if c==15:
+            print(d[1])
+        if c==16:
+            print(d[2])
+        if c==17:
+            print(d[3])
+        if c==18:
+            print(d[4])
+        if c==19:
+            print(d[5])
+        if c==20:
+            print(d[6])
+        if c==21:
+            print(d[7])
+        if c==22:
+            print(d[1])
+        if c==23:
+            print(d[2])
+        if c==24:
+            print(d[3])
+        if c==25:
+            print(d[4])
+        if c==26:
+            print(d[5])
+        if c==27:
+            print(d[6])
+        if c==28:
+            print(d[7])
+        if c==29:
+            print(d[1])
+        if c==30:
+            print(d[2])
+        if c==31:
+            print()
+        
+        else:
+            break
+            
+
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+
+        
+        
