21353a4c29c354d46fe8c75fa9138c5d79213c19
muhammed Ashker <muhammedashker3@gmail.com>
2020-12-18 13:31:52 +0530

    deleted unwanted lines

--- month.py
+++ month.py
@@ -3,23 +3,7 @@ import datetime
 def calander():
     x = datetime.datetime.now()
     print(x.strftime("%B"))
-    '''print("september")
-    print("mon\ttue\twed\tthu\tfri\tsat\tsun")
-    for i in range(1,8):
-        print(i,"\t",end=" ")
-    print()
-    for i in range(8,15):
-        print(i,"\t",end=" ")
-    print()
-    for i in range(15,22):
-        print(i,"\t",end=" ")
-    print()
-    for i in range(22,29):
-        print(i,"\t",end=" ")
-    print()
-    for i in range(29,31):
-        print(i,"\t",end=" ")
-    print()'''
+    
         
     
     
