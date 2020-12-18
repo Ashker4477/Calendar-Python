1403251c090b0f148f059da3acf35d12cd40bc33
muhammed Ashker <muhammedashker3@gmail.com>
2020-12-18 10:32:40 +0530

    choice is modified on while.py on branch master

--- while.py
+++ while.py
@@ -1,6 +1,6 @@
 import calendar,month
 while True:
-    ch=int(input("1.enter the date to find day\t\n2.show the month\t"))
+    ch=int(input("1.find week day\t\n2.the current month \t"))
     if ch==1:
         calendar.day()
     elif ch==2:
