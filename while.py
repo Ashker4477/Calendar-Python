6e42990a1668aadb1aba66527f01e2fb4f44185e
muhammed Ashker <muhammedashker3@gmail.com>
2020-12-18 09:02:05 +0530

    while decision is made

---
+++ while.py
@@ -0,0 +1,10 @@
+import calendar,month
+while True:
+    ch=int(input("1.enter the date to find day\t\n2.show the month\t"))
+    if ch==1:
+        calendar.day()
+    elif ch==2:
+        month.calander()
+    else:
+        break
+
